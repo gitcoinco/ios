632465cedadecf16c16bcca78c0c2d88692dcf48
Craig Heneveld <craig.heneveld@adeptmobi.com>
2017-11-09 19:12:14 -0700

    1.Side menu button to grab your keywords from the gicoin api 2.moved the side table storyboard to its own file (eaiser for multiple devs working on project) New dependencies -> Run ```pod install```

---
+++ Gitcoin/Model/UserKeywordResult.swift
@@ -0,0 +1,21 @@
+//
+//  UserKeywordResult.swift
+//  Gitcoin
+//
+//  Created by Craig Heneveld on 11/9/17.
+//  Copyright © 2017 Gitcoin. All rights reserved.
+//
+
+import Foundation
+import Mapper
+
+struct UserKeywordResult: Mappable {
+    
+    let status: Int?
+    let keywords: [String]?
+
+    init(map: Mapper) throws {
+        status = map.optionalFrom("status") // Optional property
+        keywords = map.optionalFrom("keywords") // Optional property
+    }
+}
