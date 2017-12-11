72d7260bc13ac59e16fe5089f89b01575c1dde03
Craig Heneveld <craig.heneveld@adeptmobi.com>
2017-12-10 19:12:26 -0700

    Version # in info screen

--- Gitcoin/UI/Info/InfoViewController.swift
+++ Gitcoin/UI/Info/InfoViewController.swift
@@ -15,6 +15,24 @@ class InfoViewController: UIViewController {
     
     @IBOutlet weak var playerView: YTPlayerView!
     
+    @IBOutlet weak var versionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var versionText = ""
+        
+        if let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+            versionText = "Version \(version) "
+        }
+        
+        if let version = Bundle.main.infoDictionary?["CFBundleVersion"] as? String {
+            versionText = "\(versionText)Build \(version)"
+        }
+        
+        self.versionLabel.text = versionText
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
